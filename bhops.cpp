5a57b946f2f3722d618aafe0e3212dad1ca702f6
Pekka Kukonlehto <pekkakukonlehto@outlook.com>
2025-01-22 18:17:31 +0200

    Working, but slow

--- bhops.cpp
+++ bhops.cpp
@@ -52,9 +52,11 @@ void BunnyHop(const HMODULE instance) noexcept
 		// on ground check
 		if (GetAsyncKeyState(VK_SPACE)) {
 			*reinterpret_cast<std::uintptr_t*>(client + offset::dwForceJump) = 65537; // force jump
-			std::this_thread::sleep_for(std::chrono::microseconds(10));
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
 			*reinterpret_cast<std::uintptr_t*>(client + offset::dwForceJump) = 256; // reset
 		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
 
 	// uninject
